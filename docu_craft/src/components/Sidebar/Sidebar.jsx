494fca138c073e962d050a27ba0a47384d18846a
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-07-07 22:18:45 +0600

    sidebar sub

--- docu_craft/src/components/Sidebar/Sidebar.jsx
+++ docu_craft/src/components/Sidebar/Sidebar.jsx
@@ -5,8 +5,10 @@ const Sidebar = ({ docs }) => {
 
   const nonRoots = Object.groupBy(
     docs.filter((doc) => doc.parent),
-    (parent) => parent
+    ({ parent }) => parent
   );
+
+  console.log("nonRoots", nonRoots);
 
   return (
     <nav className="lg:block my-10">
@@ -26,6 +28,21 @@ const Sidebar = ({ docs }) => {
                 >
                   <span class="truncate">{rootNode.title}</span>
                 </Link>
+
+                {nonRoots[rootNode.id] && (
+                  <ul role="list" className="border-l border-transparent">
+                    {nonRoots[rootNode.id].map((subRoot) => (
+                      <li key={subRoot.id}>
+                        <Link
+                          className="flex justify-between gap-2 py-1 pl-7 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
+                          href={`/docs/${rootNode.id}/${subRoot.id}`}
+                        >
+                          <span className="truncate">{subRoot.title}</span>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
               </li>
             ))}
           </ul>
