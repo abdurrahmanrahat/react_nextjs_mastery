d893b78e5b0b0b41707cce27d2f514a9e797e024
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-07-15 15:47:45 +0600

    QUI component

--- ecovista-parallel-api-route-server-component-data-fetching/src/app/[location]/@aqi/page.js
+++ ecovista-parallel-api-route-server-component-data-fetching/src/app/[location]/@aqi/page.js
@@ -1,8 +1,10 @@
+import AQIComponent from "@/components/AQIComponent"
 
-const AQIPage = () => {
-  return (
-    <div>AQIPage</div>
-  )
+// params come from /[] routes and searchParams come from query params ?lat=32.34
+const AQIPage = ({ params: { location }, searchParams: { latitude, longitude } }) => {
+    return (
+        <AQIComponent lat={latitude} lon={longitude} />
+    )
 }
 
 export default AQIPage
