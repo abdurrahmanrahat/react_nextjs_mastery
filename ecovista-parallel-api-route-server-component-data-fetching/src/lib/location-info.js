068fdb945b7e6ea620e9e182861c2343e748bd5d
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-07-15 15:29:03 +0600

    lib api utils

---
+++ ecovista-parallel-api-route-server-component-data-fetching/src/lib/location-info.js
@@ -0,0 +1,32 @@
+export const getLocationData = async (lat, lon) => {
+    try {
+        const response = await fetch(`https://us1.api-bdc.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}`)
+        const data = response.json()
+
+        return data
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+
+export const getLocationLatLongList = async () => {
+    try {
+        const response = await fetch(`http://localhost:3000/api/location`);
+        const data = await response.json();
+        return data;
+    } catch (e) {
+        console.error(e.message);
+    }
+};
+
+export const getLocationLatLong = async (locationName) => {
+    try {
+        const response = await fetch(
+            `http://localhost:3000/api/location/${locationName}`
+        );
+        const data = await response.json();
+        return data;
+    } catch (e) {
+        console.error(e.message);
+    }
+};
