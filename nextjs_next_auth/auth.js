4c9f6e14cc141521b300b42c7034a4a0ce342051
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-08-02 07:39:30 +0600

    mongodb adapter

--- nextjs_next_auth/auth.js
+++ nextjs_next_auth/auth.js
@@ -1,9 +1,10 @@
+import mongoClientPromise from "@/lib/mongoClientPromise"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
-
-
 export const { handlers: { GET, POST }, auth, signIn, signOut } = NextAuth({
+    adapter: MongoDBAdapter(mongoClientPromise, {databaseName: process.env.ENVIRONMENT}),
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
