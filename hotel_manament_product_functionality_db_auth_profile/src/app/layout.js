1a4c4ce6e59a1d34904dc8e144278375fe8637e0
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-08-02 11:28:54 +0600

    components

--- hotel_manament_product_functionality_db_auth_profile/src/app/layout.js
+++ hotel_manament_product_functionality_db_auth_profile/src/app/layout.js
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -23,7 +24,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Navbar />
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
