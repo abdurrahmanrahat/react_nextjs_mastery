db15a4a472222ba2ffa3905a9d582cb892f9a6ac
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-08-12 12:30:20 +0600

    card dynamic

--- weather_dashboard_context_hook/src/App.jsx
+++ weather_dashboard_context_hook/src/App.jsx
@@ -1,17 +1,20 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import WeatherBoard from "./components/weather/WeatherBoard";
+import { WeatherProvider } from "./providers";
 
 function App() {
   return (
-    <div className="">
-      <Header />
-      <main className="mt-20 2xl:mt-5 h-screen grid place-items-center">
-        <section>
-          <WeatherBoard />
-        </section>
-      </main>
-    </div>
+    <WeatherProvider>
+      <div className="">
+        <Header />
+        <main className="mt-20 2xl:mt-5 h-screen grid place-items-center">
+          <section>
+            <WeatherBoard />
+          </section>
+        </main>
+      </div>
+    </WeatherProvider>
   );
 }
 
