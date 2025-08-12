c0c09e65e5cfdb51a5b8da99b797724395bc97cb
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2025-08-12 16:50:09 +0600

    weather project done

--- weather_dashboard_context_hook/src/components/Header/Search.jsx
+++ weather_dashboard_context_hook/src/components/Header/Search.jsx
@@ -1,33 +1,38 @@
-import { useContext, useState } from "react";
-import SearchIcon from "../../assets/search.svg";
+import { useContext } from "react";
+// import SearchIcon from "../../assets/search.svg";
 import { LocationContext } from "../../context";
 import { getLocationByName } from "../../data/locaton.data";
+import useDebounce from "../../hooks/useDebounce";
 
 const Search = () => {
   const { setSelectedLocation } = useContext(LocationContext);
-  const [searchTerm, setSearchTerm] = useState("");
+  // const [searchTerm, setSearchTerm] = useState("");
 
-  function handleSubmit(e) {
-    e.preventDefault();
-
+  const doSearch = useDebounce((searchTerm) => {
     const fetchedLocation = getLocationByName(searchTerm);
 
     setSelectedLocation({ ...fetchedLocation });
-  }
+  }, 600);
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+
+    doSearch(value);
+  };
 
   return (
-    <form action="#" onSubmit={handleSubmit}>
+    <form action="#">
       <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
         <input
           className="bg-transparent  placeholder:text-white text-white w-full text-xs md:text-base outline-none border-none"
           type="search"
           placeholder="Search Location"
-          onChange={(e) => setSearchTerm(e.target.value)}
+          onChange={handleChange}
           required
         />
-        <button type="submit" className="cursor-pointer">
+        {/* <button type="submit" className="cursor-pointer">
           <img src={SearchIcon} />
-        </button>
+        </button> */}
       </div>
     </form>
   );
